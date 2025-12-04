6f54336fe6d86f60e69d86c81953e13ca979733f
Fede Dellavalle <fededellavalle02@gmail.com>
2025-12-04 20:12:07 -0300

    Tic tac toe

---
+++ 02-tic-tac-toe/src/App.jsx
@@ -0,0 +1,69 @@
+import { Children, use, useState } from "react";
+import confetti from "canvas-confetti";
+import "./App.css";
+import { Square } from "./components/Square";
+import { turns } from "./constants";
+import { checkWinner, checkEndGame } from "./logic/board";
+import { WinnerModal } from "./components/WinnerModal";
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(turns.X);
+
+  // Null es que no hay ganador, false es que hay empate
+  const [winner, setWinner] = useState(null);
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(turns.X);
+    setWinner(null);
+  };
+
+  const updateBoard = (index) => {
+    // No actualizamos esta posicion si ya tiene algo
+    if (board[index] || winner) return;
+
+    // Actualizar el tablero
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    // Cambiar el turno
+    const newTurn = turn === turns.X ? turns.O : turns.X;
+    setTurn(newTurn);
+
+    //Revisar si hay ganador
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      confetti();
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false);
+    }
+  };
+
+  return (
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reset del Juego</button>
+      <section className="game">
+        {board.map((square, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {square}
+            </Square>
+          );
+        })}
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === turns.X}>{turns.X}</Square>
+        <Square isSelected={turn === turns.O}>{turns.O}</Square>
+      </section>
+
+      <WinnerModal resetGame={resetGame} winner={winner} />
+    </main>
+  );
+}
+
+export default App;
